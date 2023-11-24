a69b4e3c43e848ac884a07ebf8437a334b0206ec
twennemann <84079382+twennemann@users.noreply.github.com>
2023-11-24 04:16:00 +0100

    Update

--- src/voice_recognition.py
+++ src/voice_recognition.py
@@ -15,6 +15,7 @@ import shutil
 
 speech_engine = sr.Recognizer()
 # Globale Variablen für die Länge der Audiodaten
+full_text = ""
 recording = False
 audio_data = []
 
@@ -39,16 +40,16 @@ safety_guide_path = r"C:\Users\twenn\Documents\GitHub\wav_to_text\gpt_instructio
 
 def return_to_default(label_text=False,
                       recording_button=False,
-                      progress_bar=False):
+                      return_full_text=False):
+    global full_text
     if label_text:
         recordning_label.config(text="Press the button to start recording \n"
                                 "or load an Audio File.")
     if recording_button:
         record_button.config(bg="gray")
-    #if progress_bar:
-        #window.progress['value'] = 0
-        #window.update_idletasks()
-        #progress_bar_frame.pack_forget()  # Verstecke die Progressbar beim Zurücksetzen
+    if return_full_text:
+        full_text = ""
+
 
 
 def load_audio_file():
@@ -65,16 +66,12 @@ def load_audio_file():
     if file_path.endswith(".ogg"):
         audio = AudioSegment.from_ogg(file_path)
     else:
-        audio = AudioSegment.from_wav(file_path) if file_path.endswith(".wav") else AudioSegment.from_mp3(file_path)
-    # audio.export("temp_audio.wav", format="wav")
-    #window.label.config(text="Recognition...")
-    
+        audio = AudioSegment.from_wav(file_path) if file_path.endswith(".wav") else AudioSegment.from_mp3(file_path)    
     
     # Ersetzen Sie den Aufruf von recognize_from_file durch die folgende Logik:
     audio_data = sr.AudioData(audio.raw_data, audio.frame_rate, audio.sample_width)
     threading.Thread(target=recognize_from_audio_data, args=(audio_data,)).start()
     recordning_label.config(text="Recognition: 0%")
-    # progress_bar_frame.pack(pady=20)
 
 def split_audio_into_segments(audio, min_segment_length=52000, max_segment_length=55000):
     global total_length_of_audio
@@ -105,6 +102,7 @@ def split_audio_into_segments(audio, min_segment_length=52000, max_segment_lengt
 
 
 def recognize_from_audio_data(audio_data):
+    global full_text
      # Konvertieren Sie AudioData zurück in ein AudioSegment-Objekt
     audio_segment = AudioSegment(
         audio_data.get_wav_data(),
@@ -126,10 +124,10 @@ def recognize_from_audio_data(audio_data):
 
     full_text = ' '.join(texts)
     recordning_label.config(text="Recognition complete!")
-    save_to_file(full_text)
+    # save_to_file(full_text)
 
 def from_microphone():
-    global recording, audio_data
+    global recording, audio_data, full_text
     with sr.Microphone() as source:
         recordning_label.config(text="Recording...")
         while recording:
@@ -159,7 +157,8 @@ def from_microphone():
             texts.append(text)
             update_progress(len(segment))
         full_text = ' '.join(texts)
-        save_to_file(full_text)
+        recordning_label.config(text="Recognition complete!")
+        # save_to_file(full_text)
 
 
 def start_stop_recording():
@@ -172,11 +171,17 @@ def start_stop_recording():
     else:
         recording = False
         record_button.config(bg="gray")
-        # progress['value'] = 0  # Fortschrittsbalken zurücksetzen
         window.update_idletasks()
 
-def save_to_file(text):
-    update_progress(total_length_of_audio - processed_length)  # Sollte den Balken auf 100% setzen
+def save_to_file():
+    global full_text
+    if full_text == "":
+        tk.messagebox.showerror("Fehler", "Es wurde noch keine Spracherkennung durchgeführt!")
+        return
+    elif recordning_label["text"] != "Recognition complete!":
+        tk.messagebox.showerror("Fehler", "Die Texterkennung ist noch nicht fertig.")
+        return
+    # update_progress(total_length_of_audio - processed_length)  # Sollte den Balken auf 100% setzen
     if os.path.exists(DEFAULT_PATH_TXT):
         initial_dir = DEFAULT_PATH_TXT
     else:
@@ -188,64 +193,54 @@ def save_to_file(text):
     style = style_entry.get()
     if style == "":
         style = "Zufälliger Stil"
-    blog_text = text_to_blog(text, file_name, style)
-    blog_to_picture(blog_text, style, file_name)
-    """
-    if file_name:
-        with open(file_name, 'w') as f:
-            f.write(text)
-        messagebox.showinfo("Info", "Text saved successfully!")
-    """
+    extra_instruction = extra_instruction_entry.get()
+    if extra_instruction != "":
+        extra_instruction = "\n\n Befolge Außerdem folgende Extraanweisungen:\n" + extra_instruction + "\n\n"
+    blog_text = text_to_blog(full_text, file_name, style, extra_instruction)
+    if generate_pictures_var.get():
+        # Der Haken ist gesetzt, führe text_to_picture aus
+        blog_to_picture(blog_text, style, file_name)
+    else:
+        # Der Haken ist nicht gesetzt, überspringe text_to_picture
+        pass  # oder weiterer Code, der ausgeführt werden soll, wenn der Haken nicht gesetzt ist
     messagebox.showinfo("Info", "Saving complete!")
     return_to_default(label_text=True,
                       recording_button=True,
-                      progress_bar=True)
+                      return_full_text=True)
 
 # Funktion zum Setzen der Gesamtlänge und zum Starten des Ladebalkens
 def set_total_length(length):
     global total_length_of_audio
     total_length_of_audio = length
-    #progress['maximum'] = total_length_of_audio
-
-"""
-# Funktion zum Aktualisieren des Fortschrittsbalkens
-def update_progress(length):
-    global processed_length
-    processed_length += length
-    progress['value'] = processed_length
-    percentage = 100 * (processed_length / total_length_of_audio)
-    # Planen Sie die GUI-Aktualisierung im Hauptthread
-    window.after(0, lambda: progress['value'] == processed_length)
-    window.after(0, lambda: progress_label.config(text="{:d}%".format(round(percentage))))
-"""
 
 def update_progress(length):
     global processed_length
     processed_length += length
-    # progress['value'] = processed_length
     percentage = round(100 * (processed_length / total_length_of_audio))
     recordning_label.config(text=f"Recognition: {percentage}%")
-    # progress_label.config(text=f"{percentage}%")
-    # Sie müssen die Funktion `progress_label.config` direkt aufrufen, nicht in einem lambda
-    # window.after(0, progress_label.config, {"text": f"{percentage}%"})
 
-def text_to_blog(audio_text, safe_path=None, style="Zufälliger Stil"):
+
+def text_to_blog(audio_text, safe_path=None, style="Zufälliger Stil",
+                 extra_instruction=""):
     global blog_instructions_path
     # Öffnet die Datei und liest den Inhalt
     with open(blog_instructions_path, 'r', encoding='utf-8') as file:
         blog_instructions = file.read()
 
-    # Kombiniert den Audio-Text und die Blog-Anweisungen
+    # Abrufen der Werte der Schieberegler
+    # print(sliders["temperature"].get(), sliders["top_p"].get(), sliders["frequency_penalty"].get(), sliders["presence_penalty"].get())
+
+    # Kombiniert den Audio-Text und die Blog-Anweisungen und verwendet die Werte der Schieberegler direkt
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
-            {"role": "user", "content": audio_text + "\n" + blog_instructions},
+            {"role": "user", "content": audio_text + "\n" + blog_instructions + extra_instruction},
         ],
         max_tokens=1000,  # Legen Sie die maximale Anzahl von Tokens für die Antwort fest
-        temperature=0.6,  # Steuert die Kreativität der Antwort
-        top_p=1.0,  # Steuert die Diversität der Antwort
-        frequency_penalty=0.5,  # Bestraft häufige Tokens
-        presence_penalty=0.0,  # Bestraft oder belohnt neue Tokens
+        temperature=sliders["temperature"].get(),  
+        top_p=sliders["top_p"].get(),  
+        frequency_penalty=sliders["frequency_penalty"].get(),  
+        presence_penalty=sliders["presence_penalty"].get(),  
     )
 
     blog_text = response['choices'][0]['message']['content'].strip()
@@ -337,58 +332,44 @@ def blog_to_picture(blog_text, style="Zufälliger Stil", save_path=None):
             response.raw.decode_content = True  # Stellt sicher, dass der Inhalt richtig dekodiert wird
             shutil.copyfileobj(response.raw, file)
 
+# Funktion, um den Wert des Schiebereglers zu holen (optional)
+def get_slider_value(slider):
+    return(slider.get())
 
 # %%
-"""Gui
-window = tk.Tk()
-window.title("Speech Recognition")
+"""Gui"""
 
-window.label = tk.Label(window, text="Press the button to start recording \n"
-                                     "or load an Audio File.")
-window.label.pack(pady=20)
+def toggle_style_entry():
+    if not generate_pictures_var.get():
+        enter_style_label.pack_forget()
+        style_entry.pack_forget()
+    else:
+        enter_style_label.pack(pady=(5,0))
+        style_entry.pack(pady=(0,20))
 
-# Innerhalb deiner GUI-Initialisierung
-window.progress_bar_frame = tk.Frame(window)
-window.progress = Progressbar(window.progress_bar_frame, orient=tk.HORIZONTAL, length=100, mode='determinate')
-window.progress.pack(side=tk.LEFT)
-
-window.progress_label = tk.Label(window.progress_bar_frame, text="0%")
-window.progress_label.pack(side=tk.LEFT)
-
-window.record_button = tk.Button(window, text="⬤", 
-                                 command=start_stop_recording,
-                                 bg="gray", font=("Arial", 20))
-window.record_button.pack(pady=20)
-
-load_button = tk.Button(window, text="Load Audiofile", command=load_audio_file, font=("Arial", 12))
-load_button.pack(pady=20)
-
-window.style_label = tk.Label(window, text="Enter the picture style:")
-window.style_label.pack(pady=(5,0))  # Padding für Abstand über dem Label
-
-window.style_entry = tk.Entry(window)  # Erstellen des Eingabefelds
-window.style_entry.pack(pady=(0,20))  # Padding für Abstand unter dem Eingabefeld
-
-
-
-
-window.mainloop()
-"""
 window = tk.Tk()
 window.title("Speech Recognition")
 
 
-# Erstellen eines PanedWindow-Widgets mit einer vertikalen Trennlinie
-paned_window = tk.PanedWindow(window, orient=tk.HORIZONTAL)
-paned_window.pack(fill=tk.BOTH, expand=True)
+# Haupt-PanedWindow-Widget, das vertikal aufgeteilt ist
+main_paned_window = tk.PanedWindow(window, orient=tk.VERTICAL)
+main_paned_window.pack(fill=tk.BOTH, expand=True)
+
+# Oberes PanedWindow-Widget für die linken und rechten Bereiche
+top_paned_window = tk.PanedWindow(main_paned_window, orient=tk.HORIZONTAL)
+main_paned_window.add(top_paned_window)
 
 # Linker Bereich für Speech Recognition
-left_pane = tk.Frame(paned_window, relief=tk.RAISED, borderwidth=2)
-paned_window.add(left_pane)
+left_pane = tk.Frame(top_paned_window, relief=tk.RAISED, borderwidth=2)
+top_paned_window.add(left_pane)
 
 # Rechter Bereich für KI-Request
-right_pane = tk.Frame(paned_window, relief=tk.RAISED, borderwidth=2)
-paned_window.add(right_pane)
+right_pane = tk.Frame(top_paned_window, relief=tk.RAISED, borderwidth=2)
+top_paned_window.add(right_pane)
+
+# Unterer Bereich (unter left_pane und right_pane)
+generade_pane = tk.Frame(main_paned_window, relief=tk.RAISED, borderwidth=2)
+main_paned_window.add(generade_pane)
 
 # Erstellen der Widgets für den linken Bereich
 speech_label = tk.Label(left_pane, text="Speech Recognition", font=("Arial", 16, 'bold'))
@@ -397,42 +378,75 @@ speech_label.pack(pady=20)
 recordning_label= tk.Label(left_pane, text="Press the button to start recording \n"
                                             "or load an Audio File.")
 recordning_label.pack(pady=20)
-"""
-progress_bar_frame = tk.Frame(left_pane)
-progress = Progressbar(progress_bar_frame, orient=tk.HORIZONTAL, length=100, mode='determinate')
-progress.pack(side=tk.LEFT)
 
-progress_bar_frame = tk.Frame(left_pane)
-progress = Progressbar(progress_bar_frame, orient=tk.HORIZONTAL, length=100, mode='determinate')
-progress_label = tk.Label(progress_bar_frame, text="0%")
-
-progress_label = tk.Label(progress_bar_frame, text="0%")
-progress_label.pack(side=tk.LEFT)
-# progress_bar_frame.pack(pady=20)
-"""
 record_button = tk.Button(left_pane, text="⬤", command=start_stop_recording, bg="gray", font=("Arial", 20))
 record_button.pack(pady=20)
 
 load_button = tk.Button(left_pane, text="Load Audiofile", command=load_audio_file, font=("Arial", 12))
 load_button.pack(pady=20)
-"""
-window.progress_bar_frame = tk.Frame(window)
-window.progress = Progressbar(window.progress_bar_frame, orient=tk.HORIZONTAL, length=100, mode='determinate')
-window.progress.pack(side=tk.LEFT)
 
-window.progress_label = tk.Label(window.progress_bar_frame, text="0%")
-window.progress_label.pack(side=tk.LEFT)
-"""
 
 # Erstellen der Widgets für den rechten Bereich
-style_label = tk.Label(right_pane, text="KI-Request", font=("Arial", 16, 'bold'))
-style_label.pack(pady=(20, 0))
+ki_label = tk.Label(right_pane, text="KI-Request", font=("Arial", 16, 'bold'))
+ki_label.pack(pady=(20, 0))
+# Erstellen der Widgets für den rechten Bereich
+blog_label = tk.Label(right_pane, text="Text Settings:", font=("Arial", 12, 'bold'))
+blog_label.pack(pady=(20, 0))
 
+# Initialisiere das Eingabefeld für den Bildstil
+extra_instruction_label = tk.Label(right_pane, text="Enter extra instructions:")
+extra_instruction_entry = tk.Entry(right_pane)#, width=30)
+# Zeige das Eingabefeld standardmäßig an
+extra_instruction_label.pack(pady=(5,0))
+extra_instruction_entry.pack(pady=(0,20), padx=(0,0))
+
+
+# Erstelle die Schieberegler
+sliders = {
+    "temperature": tk.Scale(right_pane, from_=0, to=1, resolution=0.1, orient="horizontal", label="Kreativität", command=lambda value: get_slider_value(sliders["temperature"])),
+    "top_p": tk.Scale(right_pane, from_=0, to=1, resolution=0.1, orient="horizontal", label="Diversität", command=lambda value: get_slider_value(sliders["top_p"])),
+    "frequency_penalty": tk.Scale(right_pane, from_=0, to=1, resolution=0.1, orient="horizontal", label="Bestrafung häufiger Tokens", command=lambda value: get_slider_value(sliders["frequency_penalty"])),
+    "presence_penalty": tk.Scale(right_pane, from_=0, to=1, resolution=0.1, orient="horizontal", label="Bestrafung/Belohnung neuer Tokens", command=lambda value: get_slider_value(sliders["presence_penalty"]))
+}
+
+# Setze Standardwerte für die Schieberegler
+sliders["temperature"].set(0.6)
+sliders["top_p"].set(1.0)
+sliders["frequency_penalty"].set(0.5)
+sliders["presence_penalty"].set(0.0)
+
+# Platziere die Schieberegler auf dem Bildschirm
+for slider in sliders.values():
+    slider.pack(pady=(5,5))
+
+# Checkbox für "Generate Pictures" mit Standardwert True
+generate_pictures_var = tk.BooleanVar(value=True)
+generate_pictures_check = tk.Checkbutton(right_pane, text="Generate Pictures", var=generate_pictures_var, command=toggle_style_entry)
+generate_pictures_check.pack(pady=(10, 0))
+
+# Initialisiere das Eingabefeld für den Bildstil
 enter_style_label = tk.Label(right_pane, text="Enter the picture style:")
-enter_style_label.pack(pady=(5,0))
-
 style_entry = tk.Entry(right_pane)
+# Zeige das Eingabefeld standardmäßig an
+enter_style_label.pack(pady=(5,0))
 style_entry.pack(pady=(0,20))
+
+generate_blog_button = tk.Button(generade_pane, text="Generate", command=save_to_file, font=("Arial", 12))
+generate_blog_button.pack(pady=20)
+
+
+"""Anpassen der Angezeigten größen"""
+# Erhöhen der Größe des rechten Bereichs
+right_pane.config(width=400)
+
+# Anpassen der Breite der Eingabefelder
+extra_instruction_entry.config(width=50)
+style_entry.config(width=50)
+
+# Anpassen der Länge der Schieberegler
+slider_length = 300  # Sie können diesen Wert anpassen, um die gewünschte Länge zu erreichen
+for slider in sliders.values():
+    slider.config(length=slider_length)
 
 # Starten der Hauptereignisschleife
 window.mainloop()
